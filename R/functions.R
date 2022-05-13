01abcb732b099259612bedcdb979fd2995eca1ca
Jen-Hsiang Ou <oumark.me@outlook.com>
2022-05-13 12:01:45 +0200

    Check connection
    Export one of the functions to check if the package can be installed

--- R/functions.R
+++ R/functions.R
@@ -1,10 +1,17 @@
-
+# roxygen2::roxygenise()
 
 ##### nt2r #####
-#' 
+#' @name nt2r
+#' @title Simulate r-scores of each training set size
 #' @description Calculate r-scores (un-target) by in parallel.
 #' @import TSDFGS
 #' @import parallel
+#' @param geno A numeric dataframe of genotype, column represent sites (genotype coding as 1, 0, -1)
+#' @param peno A numeric vector of phenotype
+#' @param nt Numeric. Number of training set size.
+#' @param n_iter Times of iteration. (default = 30)
+#' @param n_core Number of cores. (Only tested on MacOS and UNIX-like system)
+#' @return A vector of r-scores of each iteration
 #' @export
 nt2r = function(geno, peno, nt, n_iter = 30, n_core = NULL){
   
