7c2dcb465ea65aecfcd9fbd255c45c1dce8abd32
Jen-Hsiang Ou <oumark.me@outlook.com>
2022-05-13 09:46:03 +0200

    start new update 2022-05-13

--- R/functions.R
+++ R/functions.R
@@ -1,5 +1,16 @@
 
+
+##### nt2r #####
+#' 
+#' @description Calculate r-scores (un-target) by in parallel.
 #' @import TSDFGS
-.estimate_parameter = function(){
+#' @import parallel
+nt2r = function(geno, peno, nt, n_iter = 30, n_core = NULL){
   
+  # parallel cores
+  if(is.null(n_core)){n_core = round(parallel::detectCores() / 2)}
+  
+  r = unlist(parallel::mclapply(1:n_iter, function(i) return(max(TSDFGS::optTrain(geno, peno, nt)$TOPscore)), mc.cores = n_core))
+  return(r)
 }
+##### END #####
